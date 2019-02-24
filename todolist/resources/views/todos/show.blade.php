a393141a9605598bbd6395c28a932820fabc2146
ramansrsta <ramansresta@gmail.com>
2019-02-24 13:52:22 +0545

    'finalcommit'

--- todolist/resources/views/todos/show.blade.php
+++ todolist/resources/views/todos/show.blade.php
@@ -9,5 +9,14 @@
     <span class="badge badge-danger"> {{$todo->due}} </span>
     <hr>
     <p> {{$todo->body}} </p>
+
+    <br><br>
+    <a href="/todolist/public/todo/{{$todo->id}}/edit" class="btn btn-primary"> Edit </a>
+
+    {!! Form::open(['action' => ['TodosController@destroy',$todo->id],'method' => 'POST','class'=>'float-right']) !!}
+        {{ Form::hidden('_method','DELETE') }}    
+        {{ Form::bsSubmit('Delete',['class' => 'btn btn-danger']) }}
+    {!! Form::close() !!}
+
 @endsection
 
